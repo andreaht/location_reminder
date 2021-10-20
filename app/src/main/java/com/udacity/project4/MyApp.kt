19e612326faa992e3d153f79a861cb94b3e8d5d5
Andrea Pasetti <andrea.pasetti@lovatoelectric.com>
2021-10-20 11:33:55 +0200

    Aggiornamento build.grad

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -35,7 +35,7 @@ class MyApp : Application() {
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { RemindersLocalRepository(get()) }
             single { LocalDB.createRemindersDao(this@MyApp) }
         }
 
