a1aa9f839692a178c00cffb80e2f4ff5088d01df
Andrea Pasetti <andrea.pasetti@lovatoelectric.com>
2021-10-20 08:56:42 +0200

    Migrate from Kotlin synthetics to Jetpack view binding

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,17 +1,10 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
-import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityRemindersBinding
-
-//import kotlinx.android.synthetic.main.activity_reminders.*
 
 /**
  * The RemindersActivity that holds the reminders fragments
